efe1ed5304a9f3585fa902d29380d79e2eda7332
arathunku <m@4is.io>
2019-05-22 21:20:59 +0200

    Release 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FireAct.MixProject do
   def project do
     [
       app: :fire_act,
-      version: "0.1.6",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
